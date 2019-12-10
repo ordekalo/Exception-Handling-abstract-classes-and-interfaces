082853f588121a684a1f33149d5047d1847ba8e6
tareqya <35847896+tareqya@users.noreply.github.com>
2019-12-10 17:56:47 +0200

    Update Radius.java

--- src/interfaces/Radius.java
+++ src/interfaces/Radius.java
@@ -3,5 +3,7 @@ package interfaces;
 import exceptions.InvalidRadiusException;
 
 public interface Radius {
+    public static final int MAX = 800;
+    public static final int MIN = 0;
     void radiusCheck() throws InvalidRadiusException;
 }
